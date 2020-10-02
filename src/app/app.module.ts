d37b15e7097c742fbd53687727f3a206792663a8
imadmallahi <mellahi.imad1@gmail.com>
2020-10-02 19:51:05 +0100

    ajouter compenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    ProduitComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
