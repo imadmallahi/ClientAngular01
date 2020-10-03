d4ae0d0633db1a8ed9025fa43378da13e03691b7
imadmallahi <mellahi.imad1@gmail.com>
2020-10-03 13:01:51 +0100

    pak

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProduitComponent } from './produit/produit.component';
+import { ContentComponent } from './content/content.component';
+
+const routes: Routes = [
+  {path:"produit", component : ProduitComponent },
+  {path:"content", component : ContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
